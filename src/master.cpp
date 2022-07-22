55bc68e43be7ae347d360e2b583d7b0b68aa0607
Mark Harmstone <mark@harmstone.com>
2022-07-22 12:36:34 +0100

    add aes256_cbc_decrypt stub

--- src/master.cpp
+++ src/master.cpp
@@ -13,8 +13,21 @@ struct linked_server {
 	vector<uint8_t> pwdhash;
 };
 
+static vector<uint8_t> aes256_cbc_decrypt(span<const uint8_t> iv, span<const uint8_t> enc) {
+	vector<uint8_t> ret;
+
+	// FIXME
+
+	return ret;
+}
+
 void dump_master(const string& db_server, unsigned int repo, span<std::byte> smk) {
 	vector<linked_server> servs;
+
+	if (smk.size() == 16)
+		throw runtime_error("3DES SMK not supported.");
+	else if (smk.size() != 32)
+		throw runtime_error("Invaldi SMK length.");
 
 	tds::options opts(db_server, db_username, db_password);
 
@@ -33,16 +46,32 @@ JOIN master.sys.sysservers on syslnklgns.srvid=sysservers.srvid
 WHERE LEN(syslnklgns.pwdhash) > 0)");
 
 			while (sq.fetch_row()) {
-				fmt::print("!\n");
 				servs.emplace_back((string)sq[0], (string)sq[1], sq[2].val);
 			}
 		}
 	}
 
-	for (auto b : smk) {
-		fmt::print("{:02x} ", b);
+	for (const auto& serv : servs) {
+		fmt::print("srvname: {}\n", serv.srvname);
+		fmt::print("name: {}\n", serv.name);
+		fmt::print("pwdhash: ");
+
+		for (auto b : serv.pwdhash) {
+			fmt::print("{:02x} ", b);
+		}
+		fmt::print("\n");
+
+		auto hash = span(serv.pwdhash);
+		auto plaintext = aes256_cbc_decrypt(hash.subspan(4, 16), hash.subspan(20));
+
+		fmt::print("plaintext: ");
+		for (auto b : plaintext) {
+			fmt::print("{:02x} ", b);
+		}
+		fmt::print("\n");
+
+		fmt::print("\n");
 	}
-	fmt::print("\n");
 
 	// FIXME
 
