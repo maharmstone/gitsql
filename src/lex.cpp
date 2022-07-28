8402c01ab8052443d739d832607c01f1c5fff152
Mark Harmstone <mark@harmstone.com>
2022-07-28 17:15:21 +0100

    parse: use switch statement

--- src/lex.cpp
+++ src/lex.cpp
@@ -185,195 +185,263 @@ list<word> parse(string_view s) {
             w = string_view();
         }
 
-        if (c == ' ' || c == '\t' || c == '\r' || c == '\n' || c == 0xa0) {
-            if (words.empty() || words.back().type != lex::whitespace)
-                words.emplace_back(lex::whitespace, csv);
-            else
-                words.back().val = string_view(words.back().val.data(), words.back().val.size() + csv.size());
-        } else if (c == ';')
-            words.emplace_back(lex::semicolon, csv);
-        else if (c == '=')
-            words.emplace_back(lex::equals, csv);
-        else if (c == '(')
-            words.emplace_back(lex::open_bracket, csv);
-        else if (c == ')')
-            words.emplace_back(lex::close_bracket, csv);
-        else if (c == ',')
-            words.emplace_back(lex::comma, csv);
-        else if (c == '>')
-            words.emplace_back(lex::greater_than, csv);
-        else if (c == '<')
-            words.emplace_back(lex::less_than, csv);
-        else if (c == ':')
-            words.emplace_back(lex::colon, csv);
-        else if (c == '!')
-            words.emplace_back(lex::exclamation_point, csv);
-        else if (c == '*')
-            words.emplace_back(lex::asterisk, csv);
-        else if (c == '&')
-            words.emplace_back(lex::ampersand, csv);
-        else if (c == '%')
-            words.emplace_back(lex::percent, csv);
-        else if (c == '|')
-            words.emplace_back(lex::pipe, csv);
-        else if (c == '{')
-            words.emplace_back(lex::open_brace, csv);
-        else if (c == '}')
-            words.emplace_back(lex::close_brace, csv);
-        else if (c == '/') {
-            if (s.size() >= 2 && s[1] == '*') { // multi-line comment
-                auto end = s.find("*/", 2);
+        switch (c) {
+            case ' ':
+            case '\t':
+            case '\r':
+            case '\n':
+            case 0xa0:
+                if (words.empty() || words.back().type != lex::whitespace)
+                    words.emplace_back(lex::whitespace, csv);
+                else
+                    words.back().val = string_view(words.back().val.data(), words.back().val.size() + csv.size());
 
-                // FIXME - nested comments
+                break;
 
-                if (end == string::npos)
-                    throw runtime_error("Unterminated block comment.");
+            case ';':
+                words.emplace_back(lex::semicolon, csv);
+                break;
 
-                words.emplace_back(lex::multi_comment, s.substr(0, end + 2));
-                s = s.substr(end + 2);
+            case '=':
+                words.emplace_back(lex::equals, csv);
+                break;
+
+            case '(':
+                words.emplace_back(lex::open_bracket, csv);
+                break;
+
+            case ')':
+                words.emplace_back(lex::close_bracket, csv);
+                break;
+
+            case ',':
+                words.emplace_back(lex::comma, csv);
+                break;
+
+            case '>':
+                words.emplace_back(lex::greater_than, csv);
+                break;
+
+            case '<':
+                words.emplace_back(lex::less_than, csv);
+                break;
+
+            case ':':
+                words.emplace_back(lex::colon, csv);
+                break;
+
+            case '!':
+                words.emplace_back(lex::exclamation_point, csv);
+                break;
+
+            case '*':
+                words.emplace_back(lex::asterisk, csv);
+                break;
+
+            case '&':
+                words.emplace_back(lex::ampersand, csv);
+                break;
+
+            case '%':
+                words.emplace_back(lex::percent, csv);
+                break;
+
+            case '|':
+                words.emplace_back(lex::pipe, csv);
+                break;
+
+            case '{':
+                words.emplace_back(lex::open_brace, csv);
+                break;
+
+            case '}':
+                words.emplace_back(lex::close_brace, csv);
+                break;
+
+            case '/':
+                if (s.size() >= 2 && s[1] == '*') { // multi-line comment
+                    auto end = s.find("*/", 2);
+
+                    // FIXME - nested comments
+
+                    if (end == string::npos)
+                        throw runtime_error("Unterminated block comment.");
+
+                    words.emplace_back(lex::multi_comment, s.substr(0, end + 2));
+                    s = s.substr(end + 2);
+
+                    continue;
+                } else
+                    words.emplace_back(lex::slash, csv);
+
+                break;
+
+            case '[': {
+                size_t sb = 1;
+
+                do {
+                    auto sb2 = s.find(']', sb);
+
+                    if (sb2 == string::npos)
+                        throw runtime_error("Unterminated square brackets.");
+
+                    if (s.size() > sb2 + 1 && s[sb2 + 1] == ']') { // handle ]]
+                        sb = sb2 + 2;
+                        continue;
+                    }
+
+                    words.emplace_back(lex::identifier, s.substr(0, sb2 + 1));
+                    s = s.substr(sb2 + 1);
+                    break;
+                } while (true);
 
                 continue;
-            } else
-                words.emplace_back(lex::slash, csv);
-        } else if (c == '-' && s.size() > 1 && s[1] == '-') { // single-line comment
-            auto nl = s.find('\n'); // FIXME - should also be \r
-
-            if (nl == string::npos) {
-                words.emplace_back(lex::single_comment, s);
-                break;
             }
 
-            words.emplace_back(lex::single_comment, s.substr(0, nl + 1));
-            s = s.substr(nl + 1);
-            continue;
-        } else if (c == '[') {
-            size_t sb = 1;
+            case '"': {
+                size_t sb = 1;
 
-            do {
-                auto sb2 = s.find(']', sb);
+                do {
+                    auto sb2 = s.find('"', sb);
 
-                if (sb2 == string::npos)
-                    throw runtime_error("Unterminated square brackets.");
+                    if (sb2 == string::npos)
+                        throw runtime_error("Unterminated double quotation marks.");
 
-                if (s.size() > sb2 + 1 && s[sb2 + 1] == ']') { // handle ]]
-                    sb = sb2 + 2;
-                    continue;
-                }
+                    if (s.size() > sb2 + 1 && s[sb2 + 1] == '"') { // handle ""
+                        sb = sb2 + 2;
+                        continue;
+                    }
 
-                words.emplace_back(lex::identifier, s.substr(0, sb2 + 1));
-                s = s.substr(sb2 + 1);
-                break;
-            } while (true);
-
-            continue;
-        } else if (c == '"') {
-            size_t sb = 1;
-
-            do {
-                auto sb2 = s.find('"', sb);
-
-                if (sb2 == string::npos)
-                    throw runtime_error("Unterminated double quotation marks.");
-
-                if (s.size() > sb2 + 1 && s[sb2 + 1] == '"') { // handle ""
-                    sb = sb2 + 2;
-                    continue;
-                }
-
-                words.emplace_back(lex::identifier, s.substr(0, sb2 + 1));
-                s = s.substr(sb2 + 1);
-                break;
-            } while (true);
-
-            continue;
-        } else if (c == '@') { // variable
-            size_t ends = 0;
-            for (size_t i = 1; i < s.size(); i++) {
-                if ((s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a' || s[i] > 'z') && s[i] != '_' && s[i] != '#' && s[i] != '@' && s[i] != '$') {
-                    ends = i;
+                    words.emplace_back(lex::identifier, s.substr(0, sb2 + 1));
+                    s = s.substr(sb2 + 1);
                     break;
-                }
+                } while (true);
+
+                continue;
             }
 
-            if (ends == 0) {
-                words.emplace_back(lex::variable, s);
-                break;
-            }
-
-            words.emplace_back(lex::variable, s.substr(0, ends));
-            s = s.substr(ends);
-            continue;
-        } else if (c == '.' || c == '-' || c == '+' || (c >= '0' && c <= '9')) { // number
-            if (c == '0' && s.size() >= 2 && s[1] == 'x') { // binary literal
+            case '@': { // variable
                 size_t ends = 0;
-                for (size_t i = 2; i < s.size(); i++) {
-                    if ((s[i] < 'A' || s[i] > 'F') && (s[i] < 'a' || s[i] > 'f') && (s[i] < '0' || s[i] > '9')) {
+                for (size_t i = 1; i < s.size(); i++) {
+                    if ((s[i] < 'A' || s[i] > 'Z') && (s[i] < 'a' || s[i] > 'z') && s[i] != '_' && s[i] != '#' && s[i] != '@' && s[i] != '$') {
                         ends = i;
                         break;
                     }
                 }
 
                 if (ends == 0) {
-                    words.emplace_back(lex::binary_literal, s);
+                    words.emplace_back(lex::variable, s);
                     break;
                 }
 
-                words.emplace_back(lex::binary_literal, s.substr(0, ends));
+                words.emplace_back(lex::variable, s.substr(0, ends));
                 s = s.substr(ends);
                 continue;
             }
 
-            auto ends = parse_number(s);
+            case '-':
+                if (s.size() > 1 && s[1] == '-') { // single-line comment
+                    auto nl = s.find('\n'); // FIXME - should also be \r
 
-            if (ends == 0) {
-                switch (c) {
-                    case '.':
-                        words.emplace_back(lex::full_stop, string_view(s.data(), 1));
+                    if (nl == string::npos) {
+                        words.emplace_back(lex::single_comment, s);
                         break;
+                    }
 
-                    case '-':
-                        words.emplace_back(lex::minus, string_view(s.data(), 1));
-                        break;
-
-                    case '+':
-                        words.emplace_back(lex::plus, string_view(s.data(), 1));
-                        break;
-
-                    default:
-                        throw runtime_error("Unexpected error when trying to parse number.");
-
+                    words.emplace_back(lex::single_comment, s.substr(0, nl + 1));
+                    s = s.substr(nl + 1);
+                    continue;
                 }
+                [[fallthrough]];
 
-                s = s.substr(1);
-                continue;
-            }
+            // number
+            case '.':
+            case '+':
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9': {
+                if (c == '0' && s.size() >= 2 && s[1] == 'x') { // binary literal
+                    size_t ends = 0;
+                    for (size_t i = 2; i < s.size(); i++) {
+                        if ((s[i] < 'A' || s[i] > 'F') && (s[i] < 'a' || s[i] > 'f') && (s[i] < '0' || s[i] > '9')) {
+                            ends = i;
+                            break;
+                        }
+                    }
 
-            words.emplace_back(lex::number, s.substr(0, ends));
+                    if (ends == 0) {
+                        words.emplace_back(lex::binary_literal, s);
+                        break;
+                    }
 
-            s = s.substr(ends);
-            continue;
-        } else if (c == '\'') { // string literal
-            size_t sb = 1;
-
-            do {
-                auto sb2 = s.find('\'', sb);
-
-                if (sb2 == string::npos)
-                    throw runtime_error("Unterminated quotation marks.");
-
-                if (s.size() > sb2 + 1 && s[sb2 + 1] == '\'') { // handle ''
-                    sb = sb2 + 2;
+                    words.emplace_back(lex::binary_literal, s.substr(0, ends));
+                    s = s.substr(ends);
                     continue;
                 }
 
-                words.emplace_back(lex::string_literal, s.substr(0, sb2 + 1));
-                s = s.substr(sb2 + 1);
-                break;
-            } while (true);
+                auto ends = parse_number(s);
 
-            continue;
-        } else
-            throw runtime_error("Unhandled character '" + string(csv) + "'.");
+                if (ends == 0) {
+                    switch (c) {
+                        case '.':
+                            words.emplace_back(lex::full_stop, string_view(s.data(), 1));
+                            break;
+
+                        case '-':
+                            words.emplace_back(lex::minus, string_view(s.data(), 1));
+                            break;
+
+                        case '+':
+                            words.emplace_back(lex::plus, string_view(s.data(), 1));
+                            break;
+
+                        default:
+                            throw runtime_error("Unexpected error when trying to parse number.");
+
+                    }
+
+                    s = s.substr(1);
+                    continue;
+                }
+
+                words.emplace_back(lex::number, s.substr(0, ends));
+
+                s = s.substr(ends);
+                continue;
+            }
+
+            case '\'': { // string literal
+                size_t sb = 1;
+
+                do {
+                    auto sb2 = s.find('\'', sb);
+
+                    if (sb2 == string::npos)
+                        throw runtime_error("Unterminated quotation marks.");
+
+                    if (s.size() > sb2 + 1 && s[sb2 + 1] == '\'') { // handle ''
+                        sb = sb2 + 2;
+                        continue;
+                    }
+
+                    words.emplace_back(lex::string_literal, s.substr(0, sb2 + 1));
+                    s = s.substr(sb2 + 1);
+                    break;
+                } while (true);
+
+                continue;
+            }
+
+            default:
+                throw runtime_error("Unhandled character '" + string(csv) + "'.");
+        }
 
         s = s.substr(csv.size());
     }
