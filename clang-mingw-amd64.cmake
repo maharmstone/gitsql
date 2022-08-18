c24b25661c6182d6f4a1f6428c47e239742ed899
Mark Harmstone <mark@harmstone.com>
2022-08-18 12:38:32 +0100

    add clang-mingw-amd64.cmake

---
+++ clang-mingw-amd64.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_SYSTEM_NAME Windows)
+
+set(CMAKE_C_COMPILER clang)
+set(CMAKE_C_COMPILER_TARGET x86_64-w64-mingw32)
+set(CMAKE_CXX_COMPILER clang++)
+set(CMAKE_CXX_COMPILER_TARGET x86_64-w64-mingw32)
+set(CMAKE_RC_COMPILER x86_64-w64-mingw32-windres)
+set(CMAKE_SYSTEM_PROCESSOR "AMD64")
+
+string(APPEND CMAKE_C_FLAGS_INIT " -g -Xclang -gcodeview")
+string(APPEND CMAKE_CXX_FLAGS_INIT " -g -Xclang -gcodeview")
+
+set(CMAKE_EXE_LINKER_FLAGS "-static -fuse-ld=lld -lpthread -Wl,-pdb,${CMAKE_PROJECT_NAME}.pdb") # FIXME
+set(CMAKE_SHARED_LINKER_FLAGS "-static -fuse-ld=lld -lpthread -Wl,-pdb,${CMAKE_PROJECT_NAME}.pdb") # FIXME
+
+set(CMAKE_FIND_ROOT_PATH /usr/x86_64-w64-mingw32)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
