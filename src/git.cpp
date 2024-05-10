e1fcb151d6a38cf0227495266f05e6fb8ebd8843
Mark Harmstone <mark@harmstone.com>
2024-05-10 10:52:24 +0100

    add shim for out_ptr

--- src/git.cpp
+++ src/git.cpp
@@ -12,6 +12,30 @@
 #include <fcntl.h>
 #endif
 
+// FIXME - remove this once CI lets us
+#ifndef __cpp_lib_out_ptr
+template<typename T, typename Deleter>
+class out_ptr {
+public:
+	out_ptr(std::unique_ptr<T*, Deleter>& up) : up(up) {
+		ptr = up.get();
+	}
+
+	~out_ptr() {
+		if (ptr != up.get())
+			up.reset(ptr);
+	}
+
+	operator T**() {
+		return &ptr;
+	}
+
+private:
+	T* ptr;
+	std::unique_ptr<T*, Deleter>& up;
+};
+#endif
+
 using namespace std;
 
 class git_exception : public exception {
